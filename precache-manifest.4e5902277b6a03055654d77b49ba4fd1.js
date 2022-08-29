f32036e766da81ac34335df9caadf576ee6c9805
anacondamaster97 <94930897+anacondamaster97@users.noreply.github.com>
2022-08-29 13:31:27 -0700

    Updates

---
+++ precache-manifest.4e5902277b6a03055654d77b49ba4fd1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81666ccb6c2087c490b412da459bc733",
+    "url": "/index.html"
+  },
+  {
+    "revision": "877764bc70e1224d134a",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "5b312cafeb368c9d30ef",
+    "url": "/static/css/main.952b364c.chunk.css"
+  },
+  {
+    "revision": "877764bc70e1224d134a",
+    "url": "/static/js/2.84993445.chunk.js"
+  },
+  {
+    "revision": "75457b6e8da0b8a290142cfa6d28d020",
+    "url": "/static/js/2.84993445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b312cafeb368c9d30ef",
+    "url": "/static/js/main.691c6a61.chunk.js"
+  },
+  {
+    "revision": "35ec47dfee4c65c275be",
+    "url": "/static/js/runtime-main.c6e37e88.js"
+  },
+  {
+    "revision": "a4d073f2e769c7e001af78a0d2f2b0d5",
+    "url": "/static/media/Dock.a4d073f2.JPG"
+  },
+  {
+    "revision": "4a3641a98dd6539b27d6907a1d0b883a",
+    "url": "/static/media/myProfile.4a3641a9.JPG"
+  }
+]);
